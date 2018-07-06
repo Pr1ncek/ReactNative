f7f1e552b6b07bd24da64b060437586c01c39d4d
Prince K <k.prince1997@gmail.com>
2018-07-05 18:25:32 -0700

    Styling with flexbox

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, Image } from 'react-native';
+import { Text, Image, View, StyleSheet } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -8,9 +8,25 @@ export default class AlbumDetail extends Component {
     return (
       <Card>
         <CardSection>
-          <Text>{this.props.album.title}</Text>
+          <View>
+            <Image
+              source={{ uri: this.props.album.image }}
+              style={{ width: 50, height: 50 }}
+            />
+          </View>
+          <View style={styles.albumTitle}>
+            <Text>{this.props.album.title}</Text>
+            <Text>{this.props.album.artist}</Text>
+          </View>
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  albumTitle: {
+    justifyContent: 'space-around',
+    paddingLeft: 10
+  },
+});
